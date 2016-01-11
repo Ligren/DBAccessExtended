0d0ef41f730e89ef426ed808af7b195853879fb3
Vlad <Vlad>
2016-01-11 12:33:22 -0500

    Added update and delete methods

--- JlimoDataLibrary/src/com/limosys/dblib/vladtest/test/T6467063785Test.java
+++ JlimoDataLibrary/src/com/limosys/dblib/vladtest/test/T6467063785Test.java
@@ -11,31 +11,56 @@ public class T6467063785Test {
 	@Test
 	public void testOneRec() {
 		T6467063785 c = T6467063785.getDfltInstance(T6467063785AccessType.ONEREC);
-		
-		if(c.accessOneRec(1, true)) {
+
+		if (c.accessOneRec(1, true)) {
 			c.getDataSetMain().first();
 			System.out.println("My id = " + c.getId() + "; NAME = " + c.getName());
 		}
-		
+
+		//--------- add new row
 		c.addNew();
-		c.setId(9);
-		c.setName("TEst5");
+		c.setId(5);
+		c.setName("Kalabaka!");
 		try {
 			c.saveChanges();
+			System.out.println("Created");
 		} catch (SaveException e) {
 			System.out.println("Not saved !");
 			e.printStackTrace();
 		}
+
+		//--------- update row
+		c.accessOneRec(2, true);
+		c.setName("George good programmer!");
+		try {
+			c.saveChanges();
+			System.out.println("Updated");
+		} catch (SaveException e) {
+			System.out.println("Not saved !");
+			e.printStackTrace();
+		}
+		
+		//--------- delete row
+		c.accessOneRec(5, true);
+		c.getDataSetMain().deleteRow();
+		try {
+			c.saveChanges();
+			System.out.println("Deleted");
+		} catch (SaveException e) {
+			System.out.println("Not saved !");
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	@Test
 	public void testList() {
 		T6467063785 c = T6467063785.getDfltInstance(T6467063785AccessType.LIST);
 		
-		if(c.accessList(true)) {
+		if (c.accessList(true)) {
 			c.getDataSetMain().first();
 			do {
-				System.out.println(c.getId() + ", " + c.getName());
+				System.out.println("My id (from list) = " + c.getId() + "; NAME = " + c.getName());
 			} while (c.getDataSetMain().next());
 		}
 	}
